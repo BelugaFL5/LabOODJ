ddb30c442cfd58356cb264ef918d5bbd4df8a8b0
BelugaFL5 <begula123765@gmail.com>
2024-08-29 11:02:56 +0800

    update on lab 6 solutions, question 1 Person class

---
+++ src/Lab6_Q1/Person.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab6_Q1;
+
+/**
+ *
+ * @author SIOW HAN BIN
+ */
+public class Person {
+    protected String name;
+    protected String address;
+    protected String phoneNumber;
+    protected String emailAddress;
+    //initialize person object in constructor
+    public Person(
+            String name,String address,String phoneNumber,String emailAddress){
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        
+    }
+}
